684e6f31ed96a3b2e12a2c62acacc7fd40166e5e
Akhileshreddy-py <reddyakhilesh94@gmail.com>
2021-05-15 07:57:27 +0530

    3 files modified

--- index.js
+++ index.js
@@ -2,5 +2,19 @@
 import './style.css';
 
 // Write Javascript code!
+var top=0;
+var left=0;
+var snake=document.getElementById("snake");
 const appDiv = document.getElementById('app');
+
 appDiv.innerHTML = `<h1>JS Starter</h1>`;
+function ak(e)
+{
+  if(e.key=="ArrowUp")
+{
+top=top+10;
+snake.style.top=top+"px";
+}
+console.log(e.key)
+  }
+addEventListener("keydown",ak);
